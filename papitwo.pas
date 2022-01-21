3944b95b9426e55bff77779e8bcb991b22fe4df6
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-21 03:28:03 -0600

    Add base of file

---
+++ papitwo.pas
@@ -0,0 +1,24 @@
+program PapiTwo;
+
+{
+   Copyright (C) 2022  Purata Funes, Omar Jair <ventgrey@gmail.com>
+   Author: Purata Funes, Omar Jair <ventgrey@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ }
+
+uses
+    SysUtils,
+    Classes,
+    fphttpclient;
